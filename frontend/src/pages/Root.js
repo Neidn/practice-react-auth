2550c8eb0dab433d032530b5857068d97b5aa1b9
Neidn <dty3152@gmail.com>
2023-04-11 10:57:24 +0900

    modify auto logout and control by datetime

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -3,6 +3,8 @@ import {Outlet, useLoaderData, useSubmit} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
+import {getTokenDuration} from "../util/auth";
+
 function RootLayout() {
   const token = useLoaderData();
   const submit = useSubmit();
@@ -13,9 +15,16 @@ function RootLayout() {
       return;
     }
 
+    if (token === 'EXPIRED') {
+      submit(null, {action: '/logout', method: 'post'});
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
     setTimeout(() => {
       submit(null, {action: '/logout', method: 'post'});
-    }, 60 * 60 * 1000);
+    }, tokenDuration);
 
   }, [token, submit]);
 
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,7 +1,26 @@
 import {redirect} from "react-router-dom";
 
+export const getTokenDuration = () => {
+  const storageExpiration = localStorage.getItem('expiration');
+  const expiration = storageExpiration ? new Date(storageExpiration) : null;
+  const now = new Date();
+
+  return expiration.getTime() - now.getTime();
+};
+
 export const getAuthToken = () => {
   const token = localStorage.getItem('token');
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return 'EXPIRED';
+  }
+
   return token ? `Bearer ${token}` : null;
 };
 

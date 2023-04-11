f80157c6519be24bd4dd953f496b52461dceda21
Neidn <dty3152@gmail.com>
2023-04-11 10:45:18 +0900

    auto Logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,8 +1,24 @@
-import {Outlet} from 'react-router-dom';
+import {useEffect} from "react";
+import {Outlet, useLoaderData, useSubmit} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'});
+    }, 60 * 60 * 1000);
+
+  }, [token, submit]);
+
   return (
       <>
         <MainNavigation/>

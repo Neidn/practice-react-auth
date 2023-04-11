d3925ad1864fbca11a925433408019144eac8289
Neidn <dty3152@gmail.com>
2023-04-11 10:01:36 +0900

    add route protection using token

--- frontend/src/components/EventItem.js
+++ frontend/src/components/EventItem.js
@@ -1,29 +1,33 @@
-import { Link, useSubmit } from 'react-router-dom';
+import {Link, useSubmit, useRouteLoaderData} from 'react-router-dom';
 
 import classes from './EventItem.module.css';
 
-function EventItem({ event }) {
+function EventItem({event}) {
+  const token = useRouteLoaderData('root');
+
   const submit = useSubmit();
 
   function startDeleteHandler() {
     const proceed = window.confirm('Are you sure?');
 
     if (proceed) {
-      submit(null, { method: 'delete' });
+      submit(null, {method: 'delete'});
     }
   }
 
   return (
-    <article className={classes.event}>
-      <img src={event.image} alt={event.title} />
-      <h1>{event.title}</h1>
-      <time>{event.date}</time>
-      <p>{event.description}</p>
-      <menu className={classes.actions}>
-        <Link to="edit">Edit</Link>
-        <button onClick={startDeleteHandler}>Delete</button>
-      </menu>
-    </article>
+      <article className={classes.event}>
+        <img src={event.image} alt={event.title}/>
+        <h1>{event.title}</h1>
+        <time>{event.date}</time>
+        <p>{event.description}</p>
+        {token &&
+            <menu className={classes.actions}>
+              <Link to="edit">Edit</Link>
+              <button onClick={startDeleteHandler}>Delete</button>
+            </menu>
+        }
+      </article>
   );
 }
 
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import {redirect} from "react-router-dom";
+
 export const getAuthToken = () => {
   const token = localStorage.getItem('token');
   return token ? `Bearer ${token}` : null;
@@ -5,4 +7,14 @@ export const getAuthToken = () => {
 
 export const tokenLoader = async ({request, params}) => {
   return getAuthToken();
+};
+
+export const checkAuthLoader = async ({request, params}) => {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth?mode=login');
+  }
+
+  return null;
 };
